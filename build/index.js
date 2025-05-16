461748f2f475814471339331db4551c5573b2961
Eszuri <ualdo296@gmail.com>
2025-05-16 15:22:25 +0700

    all fixed

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { render, Text, useApp, useInput } from 'ink';
-import fs from "fs";
+import fs from 'fs';
 import { spawn } from 'child_process';
 import path from 'path';
 import clear from 'clear';
@@ -9,21 +9,22 @@ export default function App() {
     const [selectedIndex, setSelectedIndex] = useState(0);
     const [currentPath, setCurrentPath] = useState('');
     const [status, setStatus] = useState('');
-    const rootFolder = "D:/Anime_Ost/";
-    const full_path = path.join(rootFolder, currentPath);
+    const [trigger, SetTrigger] = useState(false);
+    const rootFolder = '/run/media/eszuri/New Volume/Anime_Ost';
+    const fullPath = path.join(rootFolder, currentPath);
     const { exit } = useApp();
     useInput((input, key) => {
         const selectedItem = folder[selectedIndex];
-        const targetPath = path.join(full_path, selectedItem);
+        const targetPath = path.join(fullPath, selectedItem);
         if (key.downArrow) {
             setSelectedIndex(prev => (prev + 1) % folder.length);
         }
         if (key.upArrow) {
             setSelectedIndex(prev => (prev - 1 + folder.length) % folder.length);
         }
-        if (input == "Enter" || key.return) {
+        if (input === 'Enter' || key.return) {
             const checkType = fs.statSync(targetPath);
-            if (selectedIndex == 0) {
+            if (selectedIndex === 0) {
                 const parent = path.dirname(currentPath);
                 setCurrentPath(path.relative(rootFolder, path.join(rootFolder, parent)));
                 return;
@@ -34,23 +35,10 @@ export default function App() {
                 setSelectedIndex(0);
             }
             if (checkType.isFile()) {
-                const playing = spawn('mpv', [
-                    // '--no-video',
-                    // '--force-window=no',
-                    targetPath
-                ]);
-                playing.stdout.on("data", (data) => {
-                    setStatus(data);
-                });
-                playing.on('close', (code) => {
-                    setStatus(`Playing Audio Has Ended (${code})`);
-                    setSelectedIndex(selectedIndex + 1);
-                    spawn('mpv', [
-                        // '--no-video',
-                        // '--force-window=no',
-                        targetPath
-                    ]);
-                });
+                SetTrigger(!trigger);
+                setTimeout(() => {
+                    setSelectedIndex(prev => prev + 1);
+                }, 100);
             }
         }
         if (key.escape) {
@@ -59,24 +47,52 @@ export default function App() {
         }
     });
     useEffect(() => {
-        console.clear();
-    }, []);
+        if (fullPath != rootFolder) {
+            spawn('vlc', ['--quiet', '--no-video', '--intf', 'dummy', '--play-and-exit',
+                fullPath + '/' + folder[selectedIndex]]).on('close', (x) => {
+                if (folder.length == selectedIndex + 1) {
+                    setSelectedIndex(0);
+                }
+                else {
+                    SetTrigger(!trigger);
+                }
+                setSelectedIndex(prev => prev + 1);
+                setStatus('play now' + x);
+            });
+        }
+        else {
+            setStatus('');
+        }
+    }, [trigger]);
     useEffect(() => {
         fs.readdir(path.join(rootFolder, currentPath), (err, data) => {
             if (err)
                 throw err;
-            setFolder(['../'].concat(data));
-            return data;
+            const sorted = data
+                .map(f => ({
+                file: f,
+                mtime: fs.statSync(path.join(fullPath, f)).mtime
+            }))
+                .sort((a, b) => a.mtime - b.mtime)
+                .map(f => f.file);
+            if (fullPath == rootFolder) {
+                setFolder(['../', ...sorted]);
+            }
+            else {
+                const filterFileType = ['.mp3', '.ogg', '..wav', '.flac'];
+                const x = sorted.filter(item => filterFileType.some(format => item.toLowerCase().endsWith(format)));
+                setFolder(['../', ...x]);
+            }
         });
-    }, [selectedIndex, currentPath]);
+    }, [currentPath]);
     return (React.createElement(React.Fragment, null,
         folder.map((value, index) => (React.createElement(Text, { color: index === selectedIndex ? 'cyan' : undefined, key: index },
-            index == selectedIndex ? " ▶️" : "   ",
+            index === selectedIndex ? ' ▶️' : '   ',
             " ",
             value))),
         React.createElement(Text, null,
             "Directory: ",
-            full_path),
+            fullPath),
         React.createElement(Text, null,
             "Selected: \"",
             folder[selectedIndex],
--- src/index.tsx
+++ src/index.tsx
@@ -1,107 +1,115 @@
 import React, {useEffect, useState} from 'react';
 import {render, Text, useApp, useInput} from 'ink';
-import fs from "fs"
+import fs from 'fs';
 import {spawn} from 'child_process';
 import path from 'path';
 import clear from 'clear';
 
 export default function App() {
-    const [folder, setFolder] = useState<string[]>([])
+    const [folder, setFolder] = useState<string[]>([]);
     const [selectedIndex, setSelectedIndex] = useState(0);
     const [currentPath, setCurrentPath] = useState('');
     const [status, setStatus] = useState('');
-    const rootFolder = "D:/Anime_Ost/"
-    const full_path = path.join(rootFolder, currentPath)
+    const [trigger, SetTrigger] = useState<boolean>(false)
+    const rootFolder = '/run/media/eszuri/New Volume/Anime_Ost';
+    const fullPath = path.join(rootFolder, currentPath);
     const {exit} = useApp();
 
-    useInput
-        ((input, key) => {
-            const selectedItem: any = folder[selectedIndex];
-            const targetPath = path.join(full_path, selectedItem);
+    useInput((input, key) => {
+        const selectedItem: any = folder[selectedIndex];
+        const targetPath = path.join(fullPath, selectedItem);
 
-            if (key.downArrow) {
-                setSelectedIndex(prev => (prev + 1) % folder.length);
+        if (key.downArrow) {
+            setSelectedIndex(prev => (prev + 1) % folder.length);
+        }
+
+        if (key.upArrow) {
+            setSelectedIndex(prev => (prev - 1 + folder.length) % folder.length);
+        }
+
+        if (input === 'Enter' || key.return) {
+            const checkType = fs.statSync(targetPath);
+
+            if (selectedIndex === 0) {
+                const parent = path.dirname(currentPath);
+                setCurrentPath(path.relative(rootFolder, path.join(rootFolder, parent)));
+                return;
             }
 
-            if (key.upArrow) {
-                setSelectedIndex(prev => (prev - 1 + folder.length) % folder.length);
+            if (checkType.isDirectory()) {
+                clear({fullClear: true});
+                setCurrentPath(path.relative(rootFolder, targetPath));
+                setSelectedIndex(0);
             }
 
-            if (input == "Enter" || key.return) {
-                const checkType = fs.statSync(targetPath);
-
-                if (selectedIndex == 0) {
-                    const parent = path.dirname(currentPath);
-                    setCurrentPath(path.relative(rootFolder, path.join(rootFolder, parent)));
-                    return;
-                }
-
-                if (checkType.isDirectory()) {
-                    clear({fullClear: true})
-                    setCurrentPath(path.relative(rootFolder, targetPath));
-                    setSelectedIndex(0)
-                }
-
-                if (checkType.isFile()) {
-                    const playing = spawn('mpv', [
-                        // '--no-video',
-                        // '--force-window=no',
-                        targetPath
-                    ]);
-
-                    playing.stdout.on("data", (data) => {
-                        setStatus(data)
-                    })
-
-                    playing.on('close', (code) => {
-                        setStatus(`Playing Audio Has Ended (${code})`);
-                        setSelectedIndex(selectedIndex + 1)
-                        spawn('mpv', [
-                            // '--no-video',
-                            // '--force-window=no',
-                            targetPath
-                        ]);
-                    })
-
-
-                }
+            if (checkType.isFile()) {
+                SetTrigger(!trigger)
+                setTimeout(() => {
+                    setSelectedIndex(prev => prev + 1)
+                }, 100)
             }
+        }
 
-            if (key.escape) {
-                exit();
-                console.clear();
-            }
-        });
+        if (key.escape) {
+            exit();
+            console.clear();
+        }
+    });
 
 
     useEffect(() => {
-        console.clear();
-    }, [])
+        if (fullPath != rootFolder) {
+            spawn('vlc', ['--quiet', '--no-video', '--intf', 'dummy', '--play-and-exit',
+                fullPath + '/' + folder[selectedIndex]]).on('close', (x) => {
+                    if (folder.length == selectedIndex + 1) {
+                        setSelectedIndex(0)
+                    } else {
+                        SetTrigger(!trigger);
+                    }
+                    setSelectedIndex(prev => prev + 1)
+                    setStatus('play now' + x)
+                })
+        } else {
+            setStatus('')
+        }
+    }, [trigger])
+
 
     useEffect(() => {
         fs.readdir(path.join(rootFolder, currentPath), (err, data) => {
-            if (err) throw err
-            setFolder(['../'].concat(data))
-            return data
-        })
-    }, [selectedIndex, currentPath])
+            if (err) throw err;
 
+            const sorted = data
+                .map(f => ({
+                    file: f,
+                    mtime: fs.statSync(path.join(fullPath, f)).mtime
+                }))
+                .sort((a: any, b: any) => a.mtime - b.mtime)
+                .map(f => f.file);
 
-
+            if (fullPath == rootFolder) {
+                setFolder(['../', ...sorted]);
+            } else {
+                const filterFileType = ['.mp3', '.ogg', '..wav', '.flac']
+                const x = sorted.filter(item => filterFileType.some(format => item.toLowerCase().endsWith(format)))
+                setFolder(['../', ...x])
+            }
+        });
+    }, [currentPath]);
 
     return (
         <>
             {folder.map((value, index) => (
-                <Text color={index === selectedIndex ? 'cyan' : undefined} key={index}>{index == selectedIndex ? " ▶️" : "   "} {value}</Text>
+                <Text color={index === selectedIndex ? 'cyan' : undefined} key={index}>
+                    {index === selectedIndex ? ' ▶️' : '   '} {value}
+                </Text>
             ))}
-
-            <Text>Directory: {full_path}</Text>
+            <Text>Directory: {fullPath}</Text>
             <Text>Selected: "{folder[selectedIndex]}"</Text>
             <Text>Status: {status}</Text>
         </>
     );
 }
 
-
-render(<App />)
+render(<App />);
 
